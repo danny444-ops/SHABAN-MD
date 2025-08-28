d66d4d467b51f7c83266d96dfb24915da0d5daa4
danny444-ops <kwartengdaniel432@gmail.com>
2025-08-28 11:22:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVUxYQ3ZQL3NqNXA5YTZkSkRWREw1QzVUYWErcnM3MlIwdWhVbVJFeERHZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicWdoVXlCdnI5czd2Ny92VVY1Z3lWa3ovUko4UDh6eXpDa3gxa1lSSmZBaz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLSTVVTlBjeEczbnM5ODB6WkRRSEtCSTZ3UTBQbGZ3aGRjeUVJZ3B4VzFFPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIzelM2SkVydDBNaGRrNmtneHg4d21vOC95ZVdEc1MzY2h1ck5xRlZtZHlJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndESWwyTlRubElkazlPdnJwR2VHaWpYRytocmt2aTA1dnZrZllvdjhvbFU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imw4eWVCdHV3YmJBa1k2WkczM3lhTE5UUFpqeFI5SkM1V1YxOTZ6OFd5VVE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ0RZYnExZ3BGa0U0QnBpdnhNa2N3Zyt2Y3NBQWpXTUVHc0hnSjlaNWhGST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYTQ4TE5IUGhGcDEyUzVUM2VITXR6YmhQTithM3BMU3pCRHpLejJVeXBYTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ikx4VnRsb3NHSzdSaC9LeDQzdVROZktxQTl2dDArZzhuQmFxSU9ZTGUzbGxNLzRoUWJ3R3RiOThJNjZwUzQzUE1VdEsxOGNCSWtUczVaYzY2YlpmS0FBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTQwLCJhZHZTZWNyZXRLZXkiOiJVallJQVdReEw2UTdTbHVHNHJkQ1AyTk9EZC9qdzNCaG43MVYvRGNTMmVBPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJOS1c1TUo2VCIsIm1lIjp7ImlkIjoiMjMzMjQwNTM3MDc0OjQwQHMud2hhdHNhcHAubmV0IiwibGlkIjoiNTA2ODU0OTYyOTU2Nzk6NDBAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNNWHg5UG9ERU5mMXdNVUdHQU1nQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiIxQWozN3VaRGV5a1hYN3d5a3J3ZjdrbUZBZE5qWWJaWEFDVXdsOHdObVFjPSIsImFjY291bnRTaWduYXR1cmUiOiJKazFnMWdzYnBJNnVNejNQYUZiT0VuY0tYWC92Q3o1K1RBQXNpdml1QlA5Q2V6dTBSMzFwSnhBbTdFT0FvMCtjRVQrVDQrQkdPbHVaeVpZeG16SFlEQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiZGd6bkhJenBNbU85dExRaVAzVVlkN1BLRUl1cHFVVUdYNmZ5VXkyblR4Q0NEUllHT0FTYllqbldBZjAzeUljeEF4Z2RyQlhVQUNEZy90Vy81UkxURHc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzMyNDA1MzcwNzQ6NDBAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZFFJOSs3bVEzc3BGMSs4TXBLOEgrNUpoUUhUWTJHMlZ3QWxNSmZNRFprSCJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FVSUFnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU2Mzc5ODY2LCJsYXN0UHJvcEhhc2giOiJubTNCYiIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBQkt1In0=",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW Sʜᴀʙᴀɴ-Mᴅ 𓅓*",
 // set the auto reply massage on status reply
@@ -19,13 +19,13 @@ WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group  
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "$",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add custom menu and mention reply image url   
 BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
+STICKER_NAME: process.env.STICKER_NAME || "dannito",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -41,7 +41,7 @@ DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜ�
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *SHABAN-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "i'm Shaban md and i'm active and running *⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -49,7 +49,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
@@ -61,7 +61,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
